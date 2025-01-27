0b4b37fe9e2042659386db9718be9b14dbac6eef
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-01-27 12:51:29 +0100

    Register pump interfaces for autoconfiguration in bundle

--- src/BenzinaBundle.php
+++ src/BenzinaBundle.php
@@ -2,11 +2,19 @@
 
 namespace Goteo\BenzinaBundle;
 
+use Goteo\BenzinaBundle\Pump\PumpInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class BenzinaBundle extends AbstractBundle {
+class BenzinaBundle extends AbstractBundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        $container->registerForAutoconfiguration(PumpInterface::class)
+            ->addTag('goteo.benzina.pump');
+    }
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.yaml');
