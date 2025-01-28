9d4446bf5b64a42cb3bced12acb3610eb0443989
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-01-28 01:52:24 +0100

    Add doctrine pump trait

---
+++ src/Pump/Trait/DoctrinePumpTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Benzina;
+
+use Doctrine\DBAL\Logging\Middleware as LoggingMiddleware;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Service\Attribute\Required;
+
+trait DoctrinePumpTrait
+{
+    private EntityManagerInterface $entityManager;
+
+    public function getEntityManager(): EntityManagerInterface
+    {
+        return $this->entityManager;
+    }
+
+    #[Required()]
+    public function setEntityManager(EntityManagerInterface $entityManager): void
+    {
+        $entityManager = clone $entityManager;
+
+        $middlewares = $entityManager->getConnection()->getConfiguration()->getMiddlewares();
+        $middlewares = \array_filter($middlewares, fn($m) => !$m instanceof LoggingMiddleware);
+
+        $entityManager->getConnection()->getConfiguration()->setMiddlewares($middlewares);
+
+        $this->entityManager = $entityManager;
+    }
+
+    public function persist(object $object): void
+    {
+        $this->entityManager->persist($object);
+
+        $this->entityManager->flush();
+        $this->entityManager->clear();
+    }
+}
